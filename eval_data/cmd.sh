cd01834a6e433f02f77561e7df139fbcc139894c
Yangyang Pu <yangyang@Yangyangs-MacBook-Air.local>
2018-02-04 14:02:41 -0800

    add parameter

--- eval_data/cmd.sh
+++ eval_data/cmd.sh
@@ -3,6 +3,9 @@
 #build_param index (no stem, have stop words)
 ../app/obj/BuildIndex build_param database.sgml
 
+#build no stem no stop words
+../app/obj/BuildIndex build_nostopw_param database.sgml
+
 #build porter stemm no stopw
 ../app/obj/BuildIndex build_stemmed_nostopw_param database.sgml
 
@@ -13,6 +16,10 @@
 #evaluate no stem, have stop words using rawtf
 ../app/obj/RetrievalEval eval_rawtf_param query
 
+#evaluate no stem no stopw
+../app/obj/RetrievalEval eval_rawtf_nostopw_param query
+
+
 #evaluate porter stem, no stopw using rawtf
 ../app/obj/RetrievalEval eval_rawtf_stemmed_nostopw_param query_stemmed_nostopw
 
@@ -21,8 +28,11 @@
 
 
 ../trec_eval qrel result_rawtf
+../trec_eval qrel result_rawtf_nostopw
 ../trec_eval qrel result_rawtf_stemmed_nostopw
 ../trec_eval qrel result_rawtf_stemmed_krovetz_nostopw
+
+
 
 
 
