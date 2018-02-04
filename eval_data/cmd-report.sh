8da61f3498d264fe2b8fa85288a2e0dafc1e307d
Yangyang Pu <yangyang@Yangyangs-MacBook-Air.local>
2018-02-04 14:59:34 -0800

    add situation

--- eval_data/cmd-report.sh
+++ eval_data/cmd-report.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
-
-../trec_eval qrel result_rawtf
+echo "output: no stem no stop words using rawtf"
 ../trec_eval qrel result_rawtf_nostopw
-../trec_eval qrel result_rawtf_stemmed_nostopw
+
+echo "output: no stem have stop words using rawtf"
+../trec_eval qrel result_rawtf
+
+echo "output: krovetz stemmer no stopw using rawtf"
 ../trec_eval qrel result_rawtf_stemmed_krovetz_nostopw
+
+echo "output: krovetz stemmer have stopw using rawtf"
+../trec_eval qrel result_rawtf_stemmed_krovetz
+
+echo "output: porter stemmer no stopw using rawtf"
+../trec_eval qrel result_rawtf_stemmed_nostopw
+
+echo "output: porter stemmer have stopw using rawtf"
+../trec_eval qrel result_rawtf_stemmed
+
--- eval_data/cmd-run.sh
+++ eval_data/cmd-run.sh
@@ -3,35 +3,44 @@
 #clean up index
 rm eval_data_* result_*
 
-#build_param index (no stem, have stop words)
-../app/obj/BuildIndex build_param database.sgml
-
 #build no stem no stop words
 ../app/obj/BuildIndex build_nostopw_param database.sgml
 
-#build porter stemm no stopw
-../app/obj/BuildIndex build_stemmed_nostopw_param database.sgml
+#build no stem have stop words
+../app/obj/BuildIndex build_param database.sgml
 
-#build krovetz stemm no stopw
+#build krovetz stemmer no stopw
 ../app/obj/BuildIndex build_stemmed_krovetz_nostopw_param database.sgml
 
+#build krovetz stemmer have stopw
+../app/obj/BuildIndex build_stemmed_krovetz_param database.sgml
 
-#evaluate no stem, have stop words using rawtf
-../app/obj/RetrievalEval eval_rawtf_param query
+#build porter stemmer no stopw
+../app/obj/BuildIndex build_stemmed_nostopw_param database.sgml
 
-#evaluate no stem no stopw
+#build porter stemmer have stopw
+../app/obj/BuildIndex build_stemmed_param database.sgml
+
+
+
+
+#evaluate no stem no stop words using rawtf
 ../app/obj/RetrievalEval eval_rawtf_nostopw_param query
 
+#evaluate no stem have stop words using rawtf
+../app/obj/RetrievalEval eval_rawtf_param query
 
-#evaluate porter stem, no stopw using rawtf
+#evaluate krovetz stemmer no stopw using rawtf
+../app/obj/RetrievalEval eval_rawtf_stemmed_krovetz_nostopw_param query
+
+#evaluate krovetz stemmer have stopw using rawtf
+../app/obj/RetrievalEval eval_rawtf_stemmed_krovetz_param query
+
+#evaluate porter stemmer no stopw using rawtf
 ../app/obj/RetrievalEval eval_rawtf_stemmed_nostopw_param query
 
-#evaluate krovetz stem no stopw
-../app/obj/RetrievalEval eval_rawtf_stemmed_krovetz_nostopw_param query_stemmed_nostopw
-
-
-
-
+#evaluate porter stemmer have stopw using rawtf
+../app/obj/RetrievalEval eval_rawtf_stemmed_param query
 
 
 
